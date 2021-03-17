6e3a83be219d4d671511fb630511d15963fb56a0
NongE <45160294+NongE@users.noreply.github.com>
2021-03-17 22:55:48 +0900

    일반 연산, 소괄호 연산에 대해 연산 기능 추가
    - 중위, 후위 표기법을 활용하여 연산 기능을 추가함

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
 
@@ -9,6 +10,10 @@ import com.example.calculator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var operatorStack = mutableListOf<String>()
+    private var numberStack = mutableListOf<Any>()
+    private var bracketFlag = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,57 +27,67 @@ class MainActivity : AppCompatActivity() {
 
         // number listener
         includeButtonView.btn0.setOnClickListener{
-            appendOnClick("0")
+            appendOnClick(false, "0")
         }
         includeButtonView.btn1.setOnClickListener{
-            appendOnClick("1")
+            appendOnClick(false, "1")
         }
         includeButtonView.btn2.setOnClickListener{
-            appendOnClick("2")
+            appendOnClick(false, "2")
         }
         includeButtonView.btn3.setOnClickListener{
-            appendOnClick("3")
+            appendOnClick(false, "3")
         }
         includeButtonView.btn4.setOnClickListener{
-            appendOnClick("4")
+            appendOnClick(false, "4")
         }
         includeButtonView.btn5.setOnClickListener{
-            appendOnClick("5")
+            appendOnClick(false, "5")
         }
         includeButtonView.btn6.setOnClickListener{
-            appendOnClick("6")
+            appendOnClick(false, "6")
         }
         includeButtonView.btn7.setOnClickListener{
-            appendOnClick("7")
+            appendOnClick(false, "7")
         }
         includeButtonView.btn8.setOnClickListener{
-            appendOnClick("8")
+            appendOnClick(false, "8")
         }
         includeButtonView.btn9.setOnClickListener{
-            appendOnClick("9")
+            appendOnClick(false, "9")
         }
 
         // operator listener
         includeButtonView.btnBracket.setOnClickListener{
-            appendOnClick("()")
+            when (bracketFlag){
+                false -> {
+                    appendOnClick(true,"(")
+                    bracketFlag = true
+                }
+                true -> {
+                    appendOnClick(true,")")
+                    bracketFlag = false
+                }
+            }
+
         }
         includeButtonView.btnPercent.setOnClickListener{
-            appendOnClick("%")
+            appendOnClick(true,"%")
         }
         includeButtonView.btnDivsion.setOnClickListener{
-            appendOnClick("/")
+            appendOnClick(true,"/")
         }
         includeButtonView.btnMulti.setOnClickListener{
-            appendOnClick("*")
+            appendOnClick(true,"*")
         }
         includeButtonView.btnMinus.setOnClickListener{
-            appendOnClick("-")
+            appendOnClick(true,"-")
         }
         includeButtonView.btnPlus.setOnClickListener{
-            appendOnClick("+")
+            appendOnClick(true,"+")
         }
         includeButtonView.btnDot.setOnClickListener{
-            appendOnClick(".")
+            appendOnClick(true,".")
         }
 
         includeButtonView.btnClear.setOnClickListener{
@@ -85,21 +100,97 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun appendOnClick(string:String){
+    private fun appendOnClick(operatorFlag: Boolean, string:String){
         val includeInputLayout = binding.IncludeInputLayout
         includeInputLayout.userInput.append(string)
+
+        if (operatorFlag){
+
+            when (string){
+                ")" -> {
+                    operatorStack.add(string)
+                    operatorStack.remove("(")
+                    operatorStack.remove(")")
+                    operatorStack.reverse()
+                    numberStack.addAll(operatorStack)
+                    operatorStack.clear()
+                }
+                else -> {
+                    operatorStack.add(string)
+                }
+            }
+
+        }else{
+            numberStack.add(string.toInt())
+        }
+
     }
 
     private fun clear(){
         val includeInputLayout = binding.IncludeInputLayout
         includeInputLayout.userInput.text = ""
         includeInputLayout.userOutput.text = ""
+        operatorStack.clear()
+        numberStack.clear()
+        bracketFlag = false
     }
 
     private fun calculate(){
-        kotlin.runCatching {
 
+        var calculateStack = mutableListOf<Int>()
+        kotlin.runCatching {
+            numberStack.addAll(operatorStack)
+
+            Log.d("calculateLog", "is operator ${operatorStack}")
+            Log.d("calculateLog", "is operator ${numberStack}")
+            for(index in 0..numberStack.size-1){
+                if(numberStack[index] is Int) {
+                    calculateStack.add(numberStack[index] as Int)
+                }else{
+                    var rangeOfCalculateStack = calculateStack.size
+                    when (numberStack[index]){
+                        "+" -> {
+                            val calTemp = calculateStack[rangeOfCalculateStack-1] + calculateStack[rangeOfCalculateStack-2]
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.add(calTemp)
+                            Log.d("calculateLog", "active + is ${calculateStack[0]}")
+                        }
+                        "-" -> {
+                            val calTemp = calculateStack[rangeOfCalculateStack-1] - calculateStack[rangeOfCalculateStack-2]
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.add(calTemp)
+                            Log.d("calculateLog", "active - is ${calculateStack[0]}")
+                        }
+                        "*" -> {
+                            val calTemp = calculateStack[rangeOfCalculateStack-1] * calculateStack[rangeOfCalculateStack-2]
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.add(calTemp)
+                            Log.d("calculateLog", "active * is ${calculateStack[0]}")
+                        }
+                        "/" -> {
+                            val calTemp = calculateStack[rangeOfCalculateStack-1] / calculateStack[rangeOfCalculateStack-2]
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.removeAt(rangeOfCalculateStack-1)
+                            rangeOfCalculateStack -= 1
+                            calculateStack.add(calTemp)
+                            Log.d("calculateLog", "active / is ${calculateStack[0]}")
+                        }
+                    }
+                }
+            }
+            binding.IncludeInputLayout.userOutput.text = "${calculateStack[0]}"
         }.onFailure {e ->
+            Log.d("calculateLog", "$e")
             Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
         }
     }
